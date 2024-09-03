4fa8555f9fe377d965f788eb6c9ba426f0871bb4
Дамир Барашев <barashev-01@mail.ru>
2024-09-03 15:29:42 +0300

    feat(): First commit

---
+++ Railway/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+const int boxPos = 891;
+
+bool isBox(int pos) {
+    return pos == boxPos;
+}
+
+int findBox(const int& start) {
+    int step = 1;
+
+    while (true) {
+        for (int x = start; x <= start + step; ++x) {
+            if (isBox(x)) {
+                return x;
+            }
+        }
+
+        for (int x = start; x >= start - step; --x) {
+            if (isBox(x)) {
+                return x;
+            }
+        }
+
+        step *= 2;
+    }
+}
+
+int main() {
+    int startPos = 0;
+
+    int boxPosition = findBox(startPos);
+
+    std::cout << "Груз найден в позиции: " << boxPosition << std::endl;
+    return 0;
+}
